02ffb66c748f9230f1c788e849642c75bdbee015
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-05-01 15:08:37 +0700

    test opening myaccount

--- Login.js
+++ Login.js
@@ -4,7 +4,7 @@ var password = document.getElementById("password");
 function check(){
     if(password.value == "password"){
         // alert("Login successfully")
-        location.href = "my-account.html";
+        location.replace("my-account.html");
         return false;
     } else {
         alert("Wrong password!");
