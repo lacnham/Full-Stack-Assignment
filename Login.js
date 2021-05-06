dd3e24033a846486dde4840b496b541b7a158bff
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2021-05-06 14:57:01 +0700

    doing 1st time user

--- Login.js
+++ Login.js
@@ -1,71 +1,95 @@
-var emailvalue = document.getElementById("email");
-var password = document.getElementById("password");
-var displayemail = document.getElementById("DisplayEmail").value;    
-var inputEmail;
-var hreff = document.getElementById("hreff");
-var UserInfo = {};
 
-// function f1(){
-//     hreff.href="Login_box.html"
+// var UserInfo = {};
+
+// // function f1(){
+// //     hreff.href="Login_box.html"
+// // }
+
+// function Emailused4login() {
+//     if(typeof(Storage) !== "undefined") {
+//         // wrapping this in a try...catch block, incase cookies are disabled
+//         // try {
+//             // Attempt to pull UserInfo (by key) from localStorage, failure results
+//         // in UserInfo being an empty object.
+//             UserInfo = JSON.parse(localStorage.getItem('UsernameLogin')) || {};
+//             // Now check if UserInfo is NOT set
+//             if(!UserInfo.UsernameLogin){
+//                 UserInfo.UsernameLogin = emailvalue.value;
+//                 localStorage.setItem('UsernameLogin',JSON.stringify(UserInfo));
+//                 displayemail = localStorage.getItem("UsernameLogin");
+
+//                 function checkpassword(password){
+//                     if(password.value == "password"){
+//                         // alert("Login successfully")   
+//                         location.href = "my-account.html";
+//                     } else {
+//                         alert("Wrong password!");
+//                     }
+//                 }
+                
+//             } else {
+//                 // UserInfo was set, welcome them back
+//                 displayemail = localStorage.getItem("UsernameLogin")
+//                 location.href = "my-account.html";   
+//             }
+        
+//         } else {
+//             alert('Your browser does not support the localStorage method!');
+//         }
+
+    // } catch(err) {
+    //     // Cookies are disabled, which threw an error, alert the user
+    //     alert('To use localStorage, you need to enable cookies.');
+    // }
 // }
 
-function Emailused4login() {
-    if(typeof(Storage) !== "undefined") {
-        // wrapping this in a try...catch block, incase cookies are disabled
-        try {
-            // Attempt to pull UserInfo (by key) from localStorage, failure results
-        // in UserInfo being an empty object.
-            UserInfo = JSON.parse(localStorage.getItem('UsernameLogin')) || {};
-            // Now check if UserInfo is NOT set
-            if(!UserInfo.UsernameLogin){
-                hreff.href="Login_box.html"; 
-                function check(){
-                    if(password.value == "password"){
-                        // alert("Login successfully")
-                        UserInfo.UsernameLogin = emailvalue.value;
-                        localStorage.setItem('UsernameLogin',JSON.stringify(UserInfo));
-                        displayemail = localStorage.getItem("UsernameLogin")
-                        location.href = "my-account.html";
-                    } else {
-                        alert("Wrong password!");
-                    }
-                }
-                       
-            } else {
-                // UserInfo was set, welcome them back
-                displayemail = localStorage.getItem("UsernameLogin")
-                location.href = "my-account.html";   
-            }
-        } else {
-            alert('Your browser does not support the localStorage method!');
-        }
 
-    } catch(err) {
-        // Cookies are disabled, which threw an error, alert the user
-        alert('To use localStorage, you need to enable cookies.');
-    }
+
+// localStorage initialization
+if (localStorage.getItem("isLoggedIn") === null) {
+    localStorage.setItem("isLoggedIn", "false");
 }
 
-// function check(){
-//     if(password.value == "password"){
-//         // alert("Login successfully")
-//         UserInfo.UsernameLogin = emailvalue.value;
-//         localStorage.setItem('UsernameLogin',JSON.stringify(UserInfo));
-//         displayemail = localStorage.getItem("UsernameLogin")
-//         window.location.href = "my-account.html";
-//     } else {
-//         alert("Wrong password!");
-//     }
-// }
+const CORRECT_PWD = "password";
+const LoginForm = document.getElementById("loginform");
 
-//       if (localStorage.clickcount < 1) {
-//         window.location.href = "Login_box.html";
-//       } else {
-//         localStorage.clickcount = 1;
-//       }
-//       document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-//     } else {
-//       document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-//     }
-//   }
+// Check entered password with correct password
+LoginForm.addEventListener("submit", function (event) {
+    // Get email and password values, and wrong password message
+    const ENTERED_EMAIL = LoginForm.username.value;
+    const ENTERED_PWD = LoginForm.password.value;
+
+    // Set email value as local storage item
+    localStorage.setItem("email", ENTERED_EMAIL);
+
+    if (ENTERED_PWD !== CORRECT_PWD) {
+        // Display wrong password message
+        alert("Wrong password!");
+        event.preventDefault();   // Stop form from submitting
+        return false;
+    } else {
+        // Set login status to 'true'
+        localStorage["isLoggedIn"] = "true";
+        return true;
+    }
+})
+
+
+
+// Get entered email on login page from local storage
+const USER_EMAIL = localStorage.getItem("email");
+
+// Get email element from my-account page
+const LOGIN_EMAIL = document.getElementById("DisplayEmail");
+
+// Change email field to entered email value
+LOGIN_EMAIL.innerHTML = USER_EMAIL;
+
+// Redirect the user back to Login page if the user hasn't logged in
+if (localStorage.isLoggedIn === null || localStorage.isLoggedIn === "false") {
+    let url = window.location.href;
+    url = url.replace("my-account.html", "Login_box.html");
+    window.location.replace(url);
+}
+
 
---
+++ shared.js
@@ -0,0 +1,40 @@
+/**
+ * Persistent log in features
+ */
+ if (localStorage["isLoggedIn"] === "true") {
+    // Mobile menu display Logged In status
+    // document.querySelector(".mobile-menu-my-account").setAttribute("style", "display: block;");
+    // document.querySelector(".mobile-menu-login").setAttribute("style", "display: none;");
+
+    // Check the login status and change Login field to 'My Account'
+    const logoutBtn = document.getElementById("logoutBtn");
+
+    if (logoutBtn) {
+        logoutBtn.setAttribute("style", "display: block");
+    }
+
+    // Redirect to My Account if the user is already logged in and tries to open either Login, Register, or Forgot Password
+    let currentURL = window.location.href;
+    if (
+        currentURL.indexOf("Login_box.html") !== -1 ||
+        currentURL.indexOf("register_acc.html") !== -1 ||
+        currentURL.indexOf("forgotPass.html") !== -1
+    ) {
+
+        // Auto redirect to "My Account" if already logged in
+        let myAccountURL = currentURL;
+        myAccountURL = myAccountURL.replace("Login_box.html", "");
+        myAccountURL = myAccountURL.replace("register_acc.html", "");
+        myAccountURL = myAccountURL.replace("forgotPass.html", "");
+        myAccountURL += "my-account.html";
+        window.location.replace(myAccountURL);
+    }
+}
+
+
+/**
+ * Logout mechanism
+ */
+function logout() {
+    localStorage["isLoggedIn"] = "false";
+}
