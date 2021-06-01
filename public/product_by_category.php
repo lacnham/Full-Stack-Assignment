15a3560bc06515d2ed34a2ecab8c6e299c4dfbc8
Nguyen Hoang To Nhu <s3878533@rmit.edu.vn>
2021-06-01 22:05:13 +0700

    fix position

--- public/product_by_category.php
+++ public/product_by_category.php
@@ -4,14 +4,8 @@
 
 <style>
     <?php include("stylesheet/browse.css")?>
-.time_filter{
-    margin-top:140px;
-    text-align: center;
-} 
 .browse{
     display: flex;
-    
-  
 }
 .line {
   display:inline-block;
@@ -44,17 +38,21 @@ img{
 #Drink{
     height: 300px;
     text-align: center;
+    padding-left: 4%;
 }
 #Pizza{
     height: 400px;
     text-align: center;
+    padding-left: 4%;
 }
 #Market{
     height: 550px;
     text-align: center;
+    padding-left: 4%;
 }
 .categories-box{
     text-align:center;
+
 }
 .btn_categories{
     text-align:center;
@@ -87,131 +85,128 @@ img{
 
 <?php $page_title="Category"; ?>
 
-<div class='pro_by_category_title'><h1>Product by categories</h1>
-<div class='categories-box'>
-    <button class='btn_categories' onclick='showfood()'>Food
-    </button>
-    <div ID="Food" style="display:none">
-    <?php
-    echo "<ul class='pro_category'>";
-    $products = read_all_products();
-    $count = 0;
-    foreach ($products as $p) {
-    $id = $p['id'];
-    $name = $p['name'];
-    $price = $p['price'];
-    $date = $p['created_time'];
-    $store = $p['store_id'];
-    if($store == 91){
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
-    echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
-    echo "<li class='pprice line'>$price</li>";
-    echo "<li class='line time'>$date</li>";
-    echo "<br>";
-    $count++;
-    if ($count == 10) {
-        break;
-    }
-    }}
-    echo '</ul>';?>
+<div class='pro_by_category_title'><h1>Product by categories</h1></div>
+<div class='center'>
+    <div class='categories-box'>
+        <button class='btn_categories' onclick='showfood()'>Food
+        </button>
+        <div ID="Food" style="display:none">
+            <?php
+            echo "<ul class='pro_category'>";
+            $products = read_all_products();
+            $count = 0;
+            foreach ($products as $p) {
+            $id = $p['id'];
+            $name = $p['name'];
+            $price = $p['price'];
+            $date = $p['created_time'];
+            $store = $p['store_id'];
+            if($store == 91){
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
+            echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
+            echo "<li class='pprice line'>$price</li>";
+            echo "<li class='line time'>$date</li>";
+            echo "<br>";
+            $count++;
+            if ($count == 10) {
+                break;
+            }
+            }}
+            echo '</ul>';?>
+        </div>
     </div>
-</div>
-
-<div class='categories-box'>
-    <button class='btn_categories' onclick='showdrink()'>Drink
-    </button>
-    <div ID="Drink" style="display:none">
-    <?php
-    echo "<ul class = 'browse'>";
-    $products = read_all_products();
-    $count = 0;
-    foreach ($products as $p) {
-    $id = $p['id'];
-    $name = $p['name'];
-    $price = $p['price'];
-    $date = $p['created_time'];
-    $store = $p['store_id'];
-    if($store == 87){
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
-    echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
-    echo "<li class='pprice line'>$price</li>";
-    echo "<li class='line time'>$date</li>";
-    echo "<br>";
-    $count++;
-    if ($count == 10) {
-        break;
-    }
-    }}
-    echo '</ul>';
-?>
-</div>
-</div>
-<div class='categories-box'>
-    <button class='btn_categories' onclick='showpizza()'>Pizza
-    </button>
-    <div ID="Pizza" style="display:none">
-    <?php
-    echo "<ul class = 'browse'>";
-    $products = read_all_products();
-    $count = 0;
-    foreach ($products as $p) {
-    $id = $p['id'];
-    $name = $p['name'];
-    $price = $p['price'];
-    $date = $p['created_time'];
-    $store = $p['store_id'];
-    if($store == 39){
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
-    echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
-    echo "<li class='pprice line'>$price</li>";
-    echo "<li class='line time'>$date</li>";
-    echo "<br>";
-    $count++;
-    if ($count == 10) {
-        break;
-    }
-    }}
-    echo '</ul>';
-?>
-</div>
-</div>
-<div class='categories-box'>
-    <button class='btn_categories' onclick='showmarket()'>Market
-    </button>
-    <div ID="Market" style="display:none">
-    <?php
-    echo "<ul class = 'browse'>";
-    $products = read_all_products();
-    $count = 0;
-    foreach ($products as $p) {
-    $id = $p['id'];
-    $name = $p['name'];
-    $price = $p['price'];
-    $date = $p['created_time'];
-    $store = $p['store_id'];
-    if($store == 75){
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
-    echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
-    echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
-    echo "<li class='pprice line'>$price</li>";
-    echo "<li class='line time'>$date</li>";
-    echo "<br>";
-    $count++;
-    if ($count == 10) {
-        break;
-    }
-    }}
-    echo '</ul>';
-?>
-</div>
-</div>
+    <div class='categories-box'>
+        <button class='btn_categories' onclick='showdrink()'>Drink
+        </button>
+        <div ID="Drink" style="display:none">
+            <?php
+            echo "<ul class = 'browse'>";
+            $products = read_all_products();
+            $count = 0;
+            foreach ($products as $p) {
+            $id = $p['id'];
+            $name = $p['name'];
+            $price = $p['price'];
+            $date = $p['created_time'];
+            $store = $p['store_id'];
+            if($store == 87){
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
+            echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
+            echo "<li class='pprice line'>$price</li>";
+            echo "<li class='line time'>$date</li>";
+            echo "<br>";
+            $count++;
+            if ($count == 10) {
+                break;
+            }
+            }}
+            echo '</ul>';?>
+        </div>
+    </div>
+    <div class='categories-box fix'>
+        <button class='btn_categories' onclick='showpizza()'>Pizza
+        </button>
+        <div ID="Pizza" style="display:none">
+        <?php
+        echo "<ul class = 'browse'>";
+        $products = read_all_products();
+        $count = 0;
+        foreach ($products as $p) {
+        $id = $p['id'];
+        $name = $p['name'];
+        $price = $p['price'];
+        $date = $p['created_time'];
+        $store = $p['store_id'];
+        if($store == 39){
+        echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
+        echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
+        echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
+        echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
+        echo "<li class='pprice line'>$price</li>";
+        echo "<li class='line time'>$date</li>";
+        echo "<br>";
+        $count++;
+        if ($count == 10) {
+            break;
+        }
+        }}
+        echo '</ul>' ?>
+        </div>
+    </div>
+    <div class='categories-box fix'>
+        <button class='btn_categories' onclick='showmarket()'>Market
+        </button>
+        <div ID="Market" style="display:none">
+            <?php
+            echo "<ul class = 'browse'>";
+            $products = read_all_products();
+            $count = 0;
+            foreach ($products as $p) {
+            $id = $p['id'];
+            $name = $p['name'];
+            $price = $p['price'];
+            $date = $p['created_time'];
+            $store = $p['store_id'];
+            if($store == 75){
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/rmitogo.png' alt='logo'></a></li>";
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\">$name</a></li>";
+            echo "<li class='line'><a class='browse_a' href=\"product.php?id=$id\"><img src='../pics_nham/winter.jpg'></a></li>";
+            echo  "<li class = 'line description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>";
+            echo "<li class='pprice line'>$price</li>";
+            echo "<li class='line time'>$date</li>";
+            echo "<br>";
+            $count++;
+            if ($count == 10) {
+                break;
+            }
+            }}
+            echo '</ul>';?>
+        </div>
+    </div>
 </div>
 <button id="up-arrow" onclick="topfunction()" title="Go to top"><i class="fa fa-arrow-circle-up"></i></button>
 <script src="addtocart.js"></script>
